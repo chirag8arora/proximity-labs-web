881e9ff2cf6ca872819dbe9b0e763e19f79f81d5
Chirag Arora <chirag.arora@zs.com>
2021-08-30 00:51:08 +0530

    added-re-chart-updated

--- src/Components/DetailedComponent.js
+++ src/Components/DetailedComponent.js
@@ -11,9 +11,9 @@ export const DetailedComponent = ({data, selectedCity}) => {
         <div className = 'model-container'>
             <h3>Live AQI Data : {selectedCity}</h3>
             <hr />
-            <LineChart width={600} height={400} data={dataToLoad}>
+            <LineChart width={600} height={400} data={dataToLoad}margin={{top: 20, right: 20, bottom: 20, left: 20}} >
                 <XAxis />
-                <YAxis dataKey="aqi" type="number" domain={['dataMin' - 50, 'dataMax' + 50]} />
+                <YAxis dataKey="aqi" type="number" domain={['dataMin', 'dataMax+10']}  />
                 <Tooltip />
                 <Legend />
                 <Line type="monotone" dataKey="aqi" stroke="#8884d8" />
